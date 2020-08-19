2ea28672798f2b0f134724a971ad756ac60545fb
Martin Chan <martinchan53@gmail.com>
2020-08-19 11:07:20 +0100

    init: initialise package with `hkdc` dataset

---
+++ R/hkdc.R
@@ -0,0 +1,30 @@
+#' Dataset with public domain information on Hong Kong District Councillors (elected 2019).
+#'
+#' A dataset containing public domain information of the 452 Hong Kong District Councillors
+#' elected in 2019.
+#'
+#' @format A data frame with 452 rows and 19 variables:
+#' \describe{
+#'   \item{ConstituencyCode}{Constituency code for the District Council}
+#'   \item{Constituency_ZH}{Constituency name in Chinese} 
+#'   \item{Constituency_EN}{Constituency name in English} 
+#'   \item{District_ZH}{District name in Chinese}     
+#'   \item{District_EN}{District name in English}     
+#'   \item{Region_ZH}{Region name in Chinese}       
+#'   \item{Region_EN}{Region name in English}       
+#'   \item{Party_ZH}{Party name in Chinese}        
+#'   \item{Party_EN}{Party name in English}        
+#'   \item{DC_ZH}{District Councillor name in Chinese}           
+#'   \item{DC_EN}{District Councillor name in English}           
+#'   \item{FacebookURL}{URL of the DC's Facebook Page}     
+#'   \item{DCPageURL}{URL of the DC's official page}       
+#'   \item{Address}{Office address of the DC}         
+#'   \item{Phone}{Phone number of the DC}           
+#'   \item{Fax}{Fax number of the DC}             
+#'   \item{Email}{Email address of the DC}           
+#'   \item{WebsiteURL}{URL of the DC's non government page}      
+#'   \item{DCProjectPageURL}{URL of the DC's page listing on Hong Kong Districts Info}
+#'   ...
+#' }
+#' @source \url{https://www.districtcouncils.gov.hk}
+"hkdc"
