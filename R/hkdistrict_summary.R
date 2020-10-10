55380697c38cd1e37b9d1b0da5a42ece914ee02b
Martin Chan <martinchan53@gmail.com>
2020-10-11 00:21:53 +0100

    docs: document new datasets

---
+++ R/hkdistrict_summary.R
@@ -0,0 +1,16 @@
+#' Dataset summarising the labels and regions of Hong Kong's Districts
+#' 
+#' This is a summary table that details the region classification and abbreviated labels
+#' of Hong Kong's districts.
+#' 
+#' @format A data frame with 18 rows and 5 variables:
+#' \describe{
+#'   \item{Code}{Constituency code for the District}
+#'   \item{District_EN}{ }
+#'   \item{District_ZH}{ } 
+#'   \item{Region_ZH}{ }
+#'   \item{Abbrev}{ }
+#'   ...
+#' }
+#' 
+"hkdistrict_summary"
--- R/hkstreetnames20.R
+++ R/hkstreetnames20.R
@@ -7,7 +7,25 @@
 #' \describe{
 #'   \item{DC}{Constituency code for the District Council}
 #'   \item{StreetNames_EN}{Street name in English} 
-#'   \item{StreetNames_ZH}{Street name in English} 
+#'   \item{StreetNames_ZH}{Street name in Chinese} 
+#'   \item{TM}{ }
+#'   \item{ST}{ }
+#'   \item{E}{ }
+#'   \item{S}{ }
+#'   \item{WC}{ }
+#'   \item{C&W}{ }
+#'   \item{Is}{ }
+#'   \item{YL}{ }
+#'   \item{SK}{ }
+#'   \item{KC}{ }
+#'   \item{YTM}{ }
+#'   \item{KT}{ }
+#'   \item{SSP}{ }
+#'   \item{N}{ }
+#'   \item{TP}{ }
+#'   \item{K&T}{ }
+#'   \item{TW}{ }
+#'   \item{WTS}{ }
 #'   ...
 #' }
 #' @source \url{https://www.landsd.gov.hk}
