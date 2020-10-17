81a2fd772bba365c4ef130ff3afba6155185069d
Martin Chan <martinchan53@gmail.com>
2020-10-17 23:02:22 +0100

    feat: add hkagepop19 dataset

---
+++ R/hkagepop19.R
@@ -0,0 +1,19 @@
+#' Land-based non-institutional population by District Council district and age group
+#'
+#' @details This dataset is updated as of 2019. Numbers represent estimates of population
+#' based on calculated census percentages. 
+#'
+#' @format A data frame with 21 rows and 8 variables:
+#' \describe{
+#'   \item{District_ZH}{}
+#'   \item{District_EN}{}
+#'   \item{Age_0_14}{}
+#'   \item{Age_15_24}{}
+#'   \item{Age_25_44}{}
+#'   \item{Age_45_64}{}
+#'   \item{Age_65}{}
+#'   \item{TotalPopulation}{}
+#'   ...
+#' }
+#' @source \url{https://www.censtatd.gov.hk/hkstat/sub/sp150.jsp?productCode=FA100096}
+"hkagepop19"
