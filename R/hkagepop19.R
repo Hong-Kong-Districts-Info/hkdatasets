b0f20a8ad5589e0e7d186496b81c56c6c14fb654
Martin Chan <martinchan53@gmail.com>
2020-11-07 18:04:08 +0000

    docs: document hkagepop19

--- R/hkagepop19.R
+++ R/hkagepop19.R
@@ -5,14 +5,14 @@
 #'
 #' @format A data frame with 18 rows and 8 variables:
 #' \describe{
-#'   \item{District_ZH}{}
-#'   \item{District_EN}{}
-#'   \item{Age_0_14}{}
-#'   \item{Age_15_24}{}
-#'   \item{Age_25_44}{}
-#'   \item{Age_45_64}{}
-#'   \item{Age_65}{}
-#'   \item{TotalPopulation}{}
+#'   \item{District_ZH}{District Name in Chinese}
+#'   \item{District_EN}{District Name in English}
+#'   \item{Age_0_14}{Population aged between 0 and 14}
+#'   \item{Age_15_24}{Population aged between 15 and 24}
+#'   \item{Age_25_44}{Population aged between 25 and 44}
+#'   \item{Age_45_64}{Population aged between 45 and 64}
+#'   \item{Age_65}{Population aged 65+}
+#'   \item{TotalPopulation}{Total population per district}
 #'   ...
 #' }
 #' @source \url{https://www.censtatd.gov.hk/hkstat/sub/sp150.jsp?productCode=FA100096}
