04e3160a70b62c14bdfc4d891e8bc3d8ac460aac
Martin Chan <martinchan53@gmail.com>
2021-07-04 11:02:47 +0100

    docs: update script for new hk_vehicles

--- .dev/script/new-traffic-data_20210508_vehicles.R
+++ .dev/script/new-traffic-data_20210508_vehicles.R
@@ -3,7 +3,6 @@ library(tidyverse)
 library(readxl)
 library(here)
 library(hkdatasets)
-library(dataCompareR)
 
 # load data ---------------------------------------------------------------
 
@@ -14,26 +13,52 @@ hk_vehicles_new <-
 
 # labels for matching up to new data
 # read each individual data frame to global environment
-# code_sheets_path <-
-#   here("data-raw",
-#        "reformatted_Code table_v1.xlsx")
-# 
-# code_sheets <- excel_sheets(code_sheets_path)
-# 
-# code_sheets %>%
-#   purrr::map(function(sheet){ # iterate through each sheet name
-#     assign(x = paste0("t_", sheet), # prefixed variable names
-#            value = readxl::read_xlsx(path = code_sheets_path, sheet = sheet),
-#            envir = .GlobalEnv)
-#   })
+code_sheets_path <-
+  here("data-raw",
+       "reformatted_Code table_v1.xlsx")
+
+code_sheets <- excel_sheets(code_sheets_path)
+
+code_sheets %>%
+  purrr::map(function(sheet){ # iterate through each sheet name
+    assign(x = paste0("t_", sheet), # prefixed variable names
+           value = readxl::read_xlsx(path = code_sheets_path, sheet = sheet),
+           envir = .GlobalEnv)
+  })
 
 # vehicles check ----------------------------------------------------------
 
-# hk_vehicles_new %>% glimpse()
-# hkdatasets::hk_vehicles %>% glimpse()
-# 
-# setdiff(names(hk_vehicles_new), names(hkdatasets::hk_vehicles))
-# setdiff(names(hkdatasets::hk_vehicles), names(hk_vehicles_new))
+hk_vehicles_new %>% glimpse()
+hkdatasets::hk_vehicles %>% glimpse()
+
+setdiff(names(hk_vehicles_new), names(hkdatasets::hk_vehicles))
+setdiff(names(hkdatasets::hk_vehicles), names(hk_vehicles_new))
+
+# comparison notes --------------------------------------------------------
+#' 1. both datasets have same number of rows.
+#' 2. new dataset has 8 additional columns. Two of those can be ignored,
+#' as they are prefixed with X (original checking columns), namely `X_Vehicle_`
+#' and `X_Duplicat`.
+#' 3. old dataset has an additional `OBJECTID` column corresponding to the
+#' row number.
+#' 3. common columns (new - old)
+#'  - `Serial_No_`
+#'  - `Year`
+#'  - `Vehicle_Cl` - `Vehicle_Class`
+#'  - `Severity_o` - `Severity_of_Accident`
+#'  - `Driver_Age`
+#'  - `Driver_Sex`
+#'  - `X_Year_of_` - `Year_of_Manufacture`
+#'  
+#' 4. new columns (new)
+#'  - `Vehicle_co`
+#'  - `First_poin`
+#'  - `Main_vehic`
+#'  - `Grid_E`
+#'  - `Grid_N`
+#'  - `Pedal_cycl`
+#'  - `Pedal_col`
+
 
 # dataCompareR ------------------------------------------------------------
 
