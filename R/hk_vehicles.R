3142006caa5c7532cfa6d0571ffebbb8831ebef9
Martin Chan <martinchan53@gmail.com>
2021-07-25 09:05:10 +0100

    docs: update variable descriptions for hk_vehicles

--- R/hk_vehicles.R
+++ R/hk_vehicles.R
@@ -9,17 +9,18 @@
 #'   \item{Serial_No_}{Numeric. Serial number for cross-matching with data
 #'   between `hk_accidents`, `hk_casualties` and `hk_vehicles`.}
 #'   \item{Year}{Numeric. Year in which incident has occurred.}
-#'   \item{Vehicle_Class}{}
-#'   \item{Vehicle_Collision}{}
-#'   \item{First_point}{}
-#'   \item{Severity_of_Accident}{}
-#'   \item{Driver_Age}{}
-#'   \item{Driver_Sex}{}
-#'   \item{Main_vehicle}{}
-#'   \item{Year_of_Manufacture}{}
-#'   \item{Grid_E}{}
-#'   \item{Grid_N}{}
-#'   \item{Pedal_cycle}{}
+#'   \item{Vehicle_Class}{Character.}
+#'   \item{Vehicle_Collision}{Character.}
+#'   \item{First_point}{Character.}
+#'   \item{Severity_of_Accident}{Character. Indicates the severity of the
+#'   accident. Can be 'Fatal', 'Serious', or 'Slight'.}
+#'   \item{Driver_Age}{Numeric. Indicates the age of the driver.}
+#'   \item{Driver_Sex}{Character. Indicates the sex of the driver.}
+#'   \item{Main_vehicle}{Character.}
+#'   \item{Year_of_Manufacture}{Character.}
+#'   \item{Grid_E}{Double.}
+#'   \item{Grid_N}{Double.}
+#'   \item{Pedal_cycle}{Character.}
 #'   ...
 #' }
 #' @source \url{https://www.td.gov.hk/}
